cd1c74b33e6f326e1dd9e552a4f3807544cf77e9
irudym <irudym@gmail.com>
2017-05-21 02:59:57 -0700

    Corrected migration

--- db/migrate/20170404152555_add_description_and_remove_mall_ref_in_shops.rb
+++ db/migrate/20170404152555_add_description_and_remove_mall_ref_in_shops.rb
@@ -2,7 +2,7 @@ class AddDescriptionAndRemoveMallRefInShops < ActiveRecord::Migration[5.0]
   def change
     add_column :shops, :description, :text
     add_reference :shops, :brand, index: true
-    remove_foreign_key :shops, :mall_id
-    remove_reference :shops, :mall, index: true
+    # remove_foreign_key :shops, :mall_id
+    # remove_reference :shops, :mall, index: true
   end
 end
