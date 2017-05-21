0555b5d03ff06e041ca385e2d695ce845eec42ed
irudym <irudym@gmail.com>
2017-05-21 08:18:27 -0700

    small UI tuninig

--- app/javascript/packs/components/header.js
+++ app/javascript/packs/components/header.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { colors } from '../styles/common_styles';
+// import { colors } from '../styles/common_styles';
 
 const headerStyle = {
   marginTop: '-7rem',
-  background: 'linear-gradient(-45deg,#4C8DB2,#84C6EC, #61BFE9)',
+  //background: 'linear-gradient(-45deg,#4C8DB2,#84C6EC, #61BFE9)',
+  borderBottom: '2px solid rgb(248, 248, 248)',
+  background: 'white',
   padding: '1rem',
   borderTopLeftRadius: '7px',
   borderTopRightRadius: '7px',
